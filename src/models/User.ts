793e8322786294d4c698e24a5cb44616c7c17844
Juan5253 <juan_sebastianf@hotmail.com>
2025-05-22 11:35:46 -0500

    Template, user model

---
+++ src/models/User.ts
@@ -0,0 +1,11 @@
+export interface User {
+  id: string;
+  title: string;
+  firstName: string;
+  lastName: string;
+  picture: string;
+  gender: string;
+  email: string;
+  dateOfBirth: string;
+  phone: string;
+}
